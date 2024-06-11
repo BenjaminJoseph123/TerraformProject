dfcb669ff458e4ea255aa0e3fd1469c0bdf10724
BenjaminJoseph123 <benjaminsamueljoseph@gmail.com>
2024-06-11 10:48:26 -0400

    terraform main

---
+++ terraform/creation/main.tf
@@ -0,0 +1,74 @@
+provider "aws" {
+  region     = "us-east-1"  # Specify your desired AWS region
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
+}
+
+resource "aws_instance" "example" {
+  ami                    = "ami-0e731c8a588258d0d" 
+  instance_type          = "t2.micro"
+  key_name               = "keypair"
+  vpc_security_group_ids = ["sg-00af089bf3a97435b"]
+  tags = {
+    Name = "react project"
+
+  }
+
+
+  connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      # private_key = base64decode(var.private_key_base64)
+      private_key = var.private_key_base64
+      host        = self.public_ip
+  }
+
+  
+  
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum update -y",
+      "mkdir project",
+      "cd project",
+      "sudo yum install git -y",
+      "git clone https://github.com/jmathew19/test.git",
+      "cd react-aws-terraform-project",
+      "sudo yum install -y nodejs npm",
+      "node --version",
+      "npm --version", 
+      "npm install web-vitals",
+      "npm install react-scripts --save-dev",
+      "npm install react-dom",
+      "npm install react-router-dom",
+      "cd ~/project/test",
+      "npm start"
+      # Additional commands to start your application
+
+    ]
+    
+    # connection {
+    #   type        = "ssh"
+    #   user        = "ec2-user"
+    #   # private_key = base64decode(var.private_key_base64)
+    #   private_key = var.private_key_base64
+    #   host        = self.public_ip
+    # }
+  }
+  
+}
+
+
+variable "private_key_base64" {
+  description = "Base64 encoded private key content"
+  type        = string
+}
+
+variable "aws_access_key_id" {
+  description = "AWS access key ID"
+  type        = string
+}
+
+variable "aws_secret_access_key" {
+  description = "AWS secret access key"
+  type        = string
+}
