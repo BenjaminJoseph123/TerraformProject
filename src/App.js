df91c5a6b836b10a11cb4b47a7cf90ff5c7659f5
BenjaminJoseph123 <benjaminsamueljoseph@gmail.com>
2024-06-11 12:22:43 -0400

    back to previous app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
-
+import Login from './Login';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Hello World
+      <Login />
       </header>
     </div>
+    
   );
 }
 
