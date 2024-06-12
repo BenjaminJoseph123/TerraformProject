c4132fb53ae7e9349079d395618810c3f4208f20
BenjaminJoseph123 <benjaminsamueljoseph@gmail.com>
2024-06-12 11:47:13 -0400

    redo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-            Edit <code>src/App.js</code> Jonny is losing his
+            Edit <code>src/App.js</code>Jonny is losing his
         </p>
         <a
           className="App-link"
