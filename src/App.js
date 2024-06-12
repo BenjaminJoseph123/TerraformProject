bca7cbed05c62390e93a138ccc7c3fdc2e515faf
BenjaminJoseph123 <benjaminsamueljoseph@gmail.com>
2024-06-12 10:58:06 -0400

    text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Justin and natalie Have Issues. If they had children they would be very big dumb
+          <code>Justin and natalie Have Issues. If they had children they would be very big dumb</code> 
         </p>
         <a
           className="App-link"
@@ -23,3 +23,4 @@ function App() {
 }
 
 export default App;
+//Edit <code>src/App.js</code> Justin and natalie Have Issues. If they had children they would be very big dumb
