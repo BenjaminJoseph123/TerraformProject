9b361a47ddcfb259634d87cd37500bb37920f173
BenjaminJoseph123 <benjaminsamueljoseph@gmail.com>
2024-06-12 11:38:04 -0400

    jonny

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-            Edit <code>src/App.js</code> Jonny is losing his
+            Jonny
         </p>
         <a
           className="App-link"
