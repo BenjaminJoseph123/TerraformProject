e5c794a1d298ff349b5b0a9e512aba2de5e35515
BenjaminJoseph123 <benjaminsamueljoseph@gmail.com>
2024-06-12 11:42:51 -0400

    try again

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-            Jonny
+            Edit <code>src/App.js</code> Jonny is losing his
         </p>
         <a
           className="App-link"
--- terraform/creation/main.tf
+++ terraform/creation/main.tf
@@ -60,6 +60,7 @@ resource "aws_instance" "example" {
 }
 
 
+
 variable "private_key_base64" {
   description = "Base64 encoded private key content"
   type        = string
