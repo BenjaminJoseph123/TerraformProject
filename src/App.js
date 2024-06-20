184fb3fc8c6511bbae44e295e23626933969f710
BenjaminJoseph123 <benjaminsamueljoseph@gmail.com>
2024-06-20 12:04:12 -0400

    changing app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit-Instance Check
+          Kombucha
         </p>
         <a
           className="App-link"
