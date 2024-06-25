6e03b1e4005cd26eb0c0dbba315cc49f0201a5dc
BenjaminJoseph123 <benjaminsamueljoseph@gmail.com>
2024-06-25 13:52:59 -0400

    better calc

--- src/App.js
+++ src/App.js
@@ -24,26 +24,38 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div className="calculator">
-          <input type="text" value={input} readOnly />
-          <input type="text" value={result} readOnly />
+          <div className="input">
+            <input type="text" value={input} readOnly />
+            <input type="text" value={result} readOnly />
+          </div>
           <div className="buttons">
-            <button onClick={() => handleClick('7')}>7</button>
-            <button onClick={() => handleClick('8')}>8</button>
-            <button onClick={() => handleClick('9')}>9</button>
-            <button onClick={() => handleClick('+')}>+</button>
-            <button onClick={() => handleClick('4')}>4</button>
-            <button onClick={() => handleClick('5')}>5</button>
-            <button onClick={() => handleClick('6')}>6</button>
-            <button onClick={() => handleClick('-')}>-</button>
-            <button onClick={() => handleClick('1')}>1</button>
-            <button onClick={() => handleClick('2')}>2</button>
-            <button onClick={() => handleClick('3')}>3</button>
-            <button onClick={() => handleClick('*')}>*</button>
-            <button onClick={() => handleClick('0')}>0</button>
-            <button onClick={() => handleClick('.')}>.</button>
-            <button onClick={() => handleClick('=')}>=</button>
-            <button onClick={() => handleClick('/')}>/</button>
-            <button onClick={() => handleClick('C')}>C</button>
+            <div className="row">
+              <button onClick={() => handleClick('7')}>7</button>
+              <button onClick={() => handleClick('8')}>8</button>
+              <button onClick={() => handleClick('9')}>9</button>
+              <button onClick={() => handleClick('/')}>/</button>
+            </div>
+            <div className="row">
+              <button onClick={() => handleClick('4')}>4</button>
+              <button onClick={() => handleClick('5')}>5</button>
+              <button onClick={() => handleClick('6')}>6</button>
+              <button onClick={() => handleClick('*')}>*</button>
+            </div>
+            <div className="row">
+              <button onClick={() => handleClick('1')}>1</button>
+              <button onClick={() => handleClick('2')}>2</button>
+              <button onClick={() => handleClick('3')}>3</button>
+              <button onClick={() => handleClick('-')}>-</button>
+            </div>
+            <div className="row">
+              <button onClick={() => handleClick('0')}>0</button>
+              <button onClick={() => handleClick('.')}>.</button>
+              <button onClick={() => handleClick('=')}>=</button>
+              <button onClick={() => handleClick('+')}>+</button>
+            </div>
+            <div className="row">
+              <button onClick={() => handleClick('C')} className="clear">C</button>
+            </div>
           </div>
         </div>
       </header>
@@ -52,5 +64,6 @@ function App() {
 }
 
 export default App;
+
 
 
