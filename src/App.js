c3163543c93439553af43d40502dfbf8351f9164
BenjaminJoseph123 <benjaminsamueljoseph@gmail.com>
2024-06-25 12:17:18 -0400

    chnage name

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Justin loves his girlfriend I mean his mom I mean his girlfriend I mean his mom
+          Justin
         </p>
         <a
           className="App-link"
