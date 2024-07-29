cf274acb11ccee61e59ea113e58d7c6ea44a8704
BenjaminJoseph123 <benjaminsamueljoseph@gmail.com>
2024-07-29 16:12:03 -0400

    dashboard2

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import { users } from './users'; // Import users array correctly
 import './LoginPage.css';
